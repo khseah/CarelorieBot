451313de47211541fb7a3d37c0aa01f65791a59e
khseah <69412944+khseah@users.noreply.github.com>
2021-07-26 18:44:56 +0800

    Update caloriefunc.py

--- functions/caloriefunc.py
+++ functions/caloriefunc.py
@@ -84,7 +84,7 @@ def add_entry(update, context):
     today = date.today()
     today = today.strftime(f"%d/%m/%Y")
     log = collection.find_one_and_update(
-        {"user": update.message.chat_id , "date": sg_time}, #query
+        {"user": update.message.chat_id , "date": today}, #query
         {'$push': {"item" : {"name":food_input, "calories": float(calorie), "protein": float(protein), "fat": float(fat), "sugar": float(sugar)}}}, #what to add to db
         upsert = True) #if theres an existing entry, add it in, else create new entry
 
